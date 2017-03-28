5978931111e70969fb4b3ad8cb5e08e0a4438cb7
shxi.he <shixi.he@weimob.com>
2017-03-28 16:55:01 +0800

    新增文件

---
+++ menu/src/components/Addres.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="addres">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addres',
+  data () {
+    return {
+      msg: 'addres',
+      data:["aaaa","bbbb","cccc"]
+    }
+  }
+}
+</script>
